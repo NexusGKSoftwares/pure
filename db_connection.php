31c951faeeae9077d1e68ecde8c6139093c1f943
NexusGk Softwares <nexusgksoftwares@gmail.com>
2024-11-05 21:28:27 +0300

    "Added db_connection.php file with database connection parameters and connection establishment code."

---
+++ db_connection.php
@@ -0,0 +1,14 @@
+<?php
+// Database connection parameters
+$servername = "localhost"; // Change this to your server name
+$username = "root"; // Change this to your database username
+$password = ""; // Change this to your database password
+$dbname = "water_management"; // Database name
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    }
+    // SQL query to select data from the table
