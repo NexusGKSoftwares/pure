7c82107ab56dcdaa38b0c5675fe5d7aaeb880016
NexusGk Softwares <nexusgksoftwares@gmail.com>
2024-11-09 10:24:32 +0300

    "Updated add_meter_reading.php to retrieve data from either $_POST or JSON input, and modified variable assignments to use the retrieved data"

--- add_meter_reading.php
+++ add_meter_reading.php
@@ -1,14 +1,21 @@
 <?php
 include 'db_connection.php';
 
+// Retrieve data based on content type
+$data = $_POST;
+if (empty($data)) {
+    // Attempt to decode JSON input if no form data is present
+    $data = json_decode(file_get_contents('php://input'), true);
+}
+
 // Debugging output to check received data
-print_r($_POST);
+print_r($data);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $user_id = isset($_POST['user_id']) ? $_POST['user_id'] : null;
-    $previous_reading = isset($_POST['previous_reading']) ? $_POST['previous_reading'] : null;
-    $current_reading = isset($_POST['current_reading']) ? $_POST['current_reading'] : null;
-    $reading_date = isset($_POST['reading_date']) ? $_POST['reading_date'] : null;
+    $user_id = isset($data['user_id']) ? $data['user_id'] : null;
+    $previous_reading = isset($data['previous_reading']) ? $data['previous_reading'] : null;
+    $current_reading = isset($data['current_reading']) ? $data['current_reading'] : null;
+    $reading_date = isset($data['reading_date']) ? $data['reading_date'] : null;
 
     if ($user_id && $previous_reading && $current_reading && $reading_date) {
         $sql = "INSERT INTO meter_readings (user_id, previous_reading, current_reading, reading_date)
